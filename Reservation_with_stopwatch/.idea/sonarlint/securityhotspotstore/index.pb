3df4c060510371e271e5467aea9eafe0a0ae92b8
Choi JunHo <duekzim@gmail.com>
2024-04-13 20:00:45 +0900

    Feat: StopWatch

---
+++ Reservation_with_stopwatch/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+Y
+)app/src/main/res/layout/activity_main.xml,7/6/76b07e40f24cce093536731477540f12e7da3cb6
+x
+Happ/src/main/java/com/example/reservation_with_stopwatch/MainActivity.kt,e/d/edd24c8705d14fe7a9466db80e5c77895255aa8f
--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var stopTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.start.setOnClickListener{onClickStart()}
+        binding.stop.setOnClickListener{onClickStop()}
+        binding.reset.setOnClickListener{onClickReset()}
+    }
+
+     private fun onClickStart() {
+        binding.timer.base = SystemClock.elapsedRealtime() + stopTime
+        binding.timer.start()
+    }
+
+    private fun onClickStop() {
+        stopTime = binding.timer.base - SystemClock.elapsedRealtime()
+        binding.timer.stop()
+    }
+    private fun onClickReset() {
+        binding.timer.base = SystemClock.elapsedRealtime()
+        stopTime = 0
     }
 }
